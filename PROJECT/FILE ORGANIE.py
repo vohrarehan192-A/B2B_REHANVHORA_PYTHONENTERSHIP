0acaeb3c3af03208f5d7c177e32c5f9268e204dc
shanavhora <shanavhora65@gmail.com>
2026-06-19 15:03:13 +0530

    first

--- PROJECT/FILE ORGANIE.py
+++ PROJECT/FILE ORGANIE.py
@@ -13,7 +13,6 @@ if not os.path.exists(folder_path):
 for file_name in os.listdir(folder_path):
     file_path = os.path.join(folder_path, file_name)
 
-    # Skip directories
     if os.path.isdir(file_path):
         continue
 
